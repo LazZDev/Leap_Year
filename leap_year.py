4bef2b32e6dca9328be8b9e8e6fdb7241d418e29
lazaromasot <lazz84@hotmail.com>
2023-09-24 23:09:42 -0400

    Changed the print statement text

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,6 @@
 year = int(input("Which year do you want to check? "))
 
 if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print(f"The year {year} is a leap year")
+    print("Leap year.")
 else:
-    print(f"The year {year} is not a leap year")
+    print("Not a leap year.")
